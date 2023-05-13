70d3ac8bb0d57ebabfaac8c9cc8d0923f5b949a1
jesse <jesse.kibaara@student.moringaschool.com>
2023-05-13 16:13:50 +0300

    new show route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  # existing routes here
+
+  resources :students, only: [:index, :show]
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+
+  # new route for individual student
+  get '/students/:id', to: 'students#show', as: 'student'
 end
